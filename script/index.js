42e439d0d3ceab4a670d06681c34cb6061d3b81e
Isaac Lourenço <isaacmarlondasilvalourenco@gmail.com>
2023-02-07 23:59:20 -0300

    feat: menu on mobile

--- script/index.js
+++ script/index.js
@@ -2,26 +2,36 @@ const nav = document.querySelector("nav");
 let lastScrollY = 0;
 const btnMenu = document.querySelector("#btnMenu");
 let isMenuOpen = false;
+const navLinks = document.querySelectorAll("nav a");
 
-function isMenuMobileShow() {
-    return btnMenu.style.display === "none"
+function isMobileOpen() {
+    return nav.classList.contains("mobileOpen");
 }
 
-window.addEventListener("scroll", () => {
+function updateTopPageStyle() {
     const scrollY = window.scrollY;
     if (scrollY > lastScrollY) {
-        nav.classList.remove("topPage")
-    } else {
+        nav.classList.remove("topPage");
+    } else if (!isMobileOpen()) {
         nav.classList.add("topPage");
     }
-});
+}
 
-btnMenu.addEventListener("click", () => {
+function toggleMenu() {
     if (!isMenuOpen) {
-        btnMenu.classList.add("open");
+        nav.classList.add("mobileOpen");
+        nav.classList.remove("topPage");
     } else {
-        btnMenu.classList.remove("open");
+        nav.classList.remove("mobileOpen");
     }
 
+    updateTopPageStyle();
     isMenuOpen = !isMenuOpen;
+}
+
+navLinks.forEach(navLink => {
+    navLink.addEventListener("click", toggleMenu);
 });
+
+window.addEventListener("scroll", updateTopPageStyle);
+btnMenu.addEventListener("click", toggleMenu);
